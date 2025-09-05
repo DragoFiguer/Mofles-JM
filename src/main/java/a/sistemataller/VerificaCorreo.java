da7d6ee16c860aecdfb07019312cc326fbbe558f
DragoFiguer <figuer.pc@gmail.com>
2025-09-04 18:02:53 -0600

    Se crea clase para verificar el correo

---
+++ src/main/java/a/sistemataller/VerificaCorreo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package a.sistemataller;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class VerificaCorreo {
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+    
+    public static boolean isValidEmail(String email){
+        Pattern p = Pattern.compile(EMAIL_REGEX);
+        Matcher m = p.matcher(email);
+        return false;
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa tu email: ");
+        String email = sc.nextLine();
+        
+        System.out.println("El correo " + email + " es: " + (isValidEmail(email) ? " valido" : " invalido"));
+    }
+    
+}
+
